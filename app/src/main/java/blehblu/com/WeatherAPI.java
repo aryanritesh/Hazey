2b2c7e1f086a7f21bf82b2c3e1aea08e0ed521f0
Aryan Ritesh <aryanrtsh@gmail.com>
2021-10-09 16:15:06 +0530

    Complete but has bugs that need to be fixed

---
+++ app/src/main/java/blehblu/com/WeatherAPI.java
@@ -0,0 +1,13 @@
+package blehblu.com;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherAPI {
+    @GET("weather?appid=a46848ae4d9293b1c1a7096d73391e39")
+    Call<OpenWeatherMap>getWeatherWithLocation(@Query("lat")double lat,@Query("lon")double lon);
+
+    @GET("weather?appid=a46848ae4d9293b1c1a7096d73391e39")
+    Call<OpenWeatherMap>getWeatherWithCity(@Query("q")String name);
+}
--- app/src/main/java/blehblu/com/WeatherAct.java
+++ app/src/main/java/blehblu/com/WeatherAct.java
@@ -3,12 +3,84 @@ package blehblu.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WeatherAct extends AppCompatActivity {
-
+    private TextView cityW,tempW,weatherConditionW,humidityWeather,maxTempWeather,minTempWeather,pressureWeather,windWeather;
+    private ImageView imageViewWeather;
+    private Button search;
+    private EditText location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        cityW=findViewById(R.id.cityW);
+        tempW=findViewById(R.id.tempW);
+        weatherConditionW=findViewById(R.id.weatherConditionW);
+        humidityWeather=findViewById(R.id.humidityWeather);
+        maxTempWeather=findViewById(R.id.maxTempWeather);
+        minTempWeather=findViewById(R.id.minTempWeather);
+        pressureWeather=findViewById(R.id.pressureWeather);
+        windWeather=findViewById(R.id.windWeather);
+        imageViewWeather=findViewById(R.id.imageViewWeather);
+        search=findViewById(R.id.search);
+        location=findViewById(R.id.citySearch);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cityName=location.getText().toString();
+            getWeather(cityName);
+            location.setText("");
+            }
+        });
+    }
+    public void getWeather(String name){
+        WeatherAPI weatherAPI= RetrofitWeather.getClient().create(WeatherAPI.class);
+        Call<OpenWeatherMap> call=weatherAPI.getWeatherWithCity(name);
+
+        call.enqueue(new Callback<OpenWeatherMap>() {
+            @Override
+            public void onResponse(Call<OpenWeatherMap> call, Response<OpenWeatherMap> response) {
+                if(response.isSuccessful()) {
+                    cityW.setText(response.body().getName() + ", " + response.body().getSys().getCountry());
+                    tempW.setText(response.body().getMain().getTemp() + "°C");
+                    weatherConditionW.setText(response.body().getWeather().get(0).getDescription());
+                    humidityWeather.setText(" : " + response.body().getMain().getHumidity());
+                    maxTempWeather.setText(" : " + response.body().getMain().getTempMax() + "°C");
+                    minTempWeather.setText(" :" + response.body().getMain().getTempMin() + "°C");
+                    pressureWeather.setText(" : " + response.body().getMain().getPressure());
+                    windWeather.setText(" : " + response.body().getWind().getSpeed());
+
+                    String iconCode = response.body().getWeather().get(0).getIcon();
+                    Picasso.get().load("https://openweathermap.org/img/wn/" + iconCode + "@2x.png")
+                            .placeholder(R.drawable.ic_launcher_background).into(imageViewWeather);
+                }
+                else
+                {
+                    Toast.makeText(WeatherAct.this,"City not found",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<OpenWeatherMap> call, Throwable t) {
+
+            }
+        });
+
     }
 }
